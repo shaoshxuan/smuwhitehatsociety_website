10027f1434b3ec2d36277ad6bb2acce28c326394
shaoshxuan <shaoshxuan.chua@gmail.com>
2020-06-22 11:52:45 +0800

    Updated Mobile View on Articles

--- src/components/articlesPage/articles/articleLayout/articleLayout.js
+++ src/components/articlesPage/articles/articleLayout/articleLayout.js
@@ -5,24 +5,22 @@ import './articleLayout.css';
 const ArticleLayout = (props) => {
     return (
         <div className="articleContainer">
-            <Link to={`/articles/${props.slug}`}>
-                <div className="articleLayout">
-                    <div>
-                        <div className="articleType">
-                            {props.type}
-                        </div>
-                        <div style={{padding: "0.3rem 0"}}></div>
-                        <div className="articleTitle">
-                            {props.title}
-                        </div>
+            <Link to={`/articles/${props.slug}`} className="articleLayout">
+                <div>
+                    <div className="articleType">
+                        {props.type}
                     </div>
-                    <div>
-                        <div className="author">
-                            <span style={{color: "#f3f1f1"}}>Author:</span><br/>{props.author}
-                        </div>
-                        <div className="date">
-                        <span style={{color: "#f3f1f1"}}>Date Posted:</span><br/>{props.datePosted}
-                        </div>
+                    <div style={{padding: "0.3rem 0"}}></div>
+                    <div className="articleTitle">
+                        {props.title}
+                    </div>
+                </div>
+                <div>
+                    <div className="author">
+                        <span style={{color: "#f3f1f1"}}>Author:</span><br/>{props.author}
+                    </div>
+                    <div className="date">
+                    <span style={{color: "#f3f1f1"}}>Date Posted:</span><br/>{props.datePosted}
                     </div>
                 </div>
             </Link>
